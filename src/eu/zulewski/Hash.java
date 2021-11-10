4256aecdcc60fc5437c180cca2a44feb1d0eaea4
Paweł Żulewski <pazulewski@gmail.com>
2021-11-10 01:38:06 +0100

    added hexToString(byte[] hex) and getByteArray(Block block) methods

--- src/eu/zulewski/Hash.java
+++ src/eu/zulewski/Hash.java
@@ -1,5 +1,31 @@
 package eu.zulewski;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Hash {
 
+    public static String getHash(Block block) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        byte[] encodedHash = messageDigest.digest(getByteArray(block));
+
+        return hexToString(encodedHash);
+    }
+
+    private static String hexToString(byte[] hex) {
+        BigInteger number = new BigInteger(1, hex);
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+
+        while (hexString.length() < 32) {
+            hexString.insert(0, '0');
+        }
+        return hexString.toString();
+    }
+
+    private static byte[] getByteArray(Block block) {
+        String str = block.data + block.hash + block.timestamp;
+        return str.getBytes(StandardCharsets.UTF_8);
+    }
 }
